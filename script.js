3165a45a58ffbcafbd07bbce000ca36dcde8e45d
druida08 <druida0088@gmail.com>
2023-10-22 20:18:19 -0500

    se agrega nuevo mensaje

--- script.js
+++ script.js
@@ -13,7 +13,8 @@ document.addEventListener("DOMContentLoaded", function () {
         const edad = document.getElementById("edad").value;
         const hobby = document.getElementById("hobby").value;
 
-        const cuento = `Había una vez una persona llamada ${nombresApellidos}, pero todos l@ conocían como ${apodo}. Tenía el cabello de color ${colorCabello} y unos hermosos ojos de color ${colorOjos}. A sus ${edad} años, su pasatiempo favorito era ${hobby}. Un día, mientras practicaba su hobby, sucedió algo inesperado...`;
+        const cuento = `Había una vez una persona llamada ${nombresApellidos}, pero todos la conocían como ${apodo}. Tenía el cabello de color ${colorCabello} y unos hermosos ojos de color ${colorOjos}. A sus ${edad} años, su pasatiempo favorito era ${hobby}. Un día, mientras practicaba su hobby, algo extraordinario sucedió. ${nombresApellidos} se encontró en un lugar mágico y desconocido, donde todo era posible. Allí, vivió aventuras inimaginables, conoció a personajes fantásticos y descubrió secretos que cambiarían su vida para siempre. Después de una emocionante travesía, ${nombresApellidos} regresó a su mundo, pero con el corazón lleno de experiencias asombrosas y recuerdos inolvidables. Y así, ${nombresApellidos} siguió adelante con su vida, sabiendo que la magia y la aventura siempre la esperarían en cada esquina.`;
+
 
         cuentoResult.textContent = cuento;
     });
